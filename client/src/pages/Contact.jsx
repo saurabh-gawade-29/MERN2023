e902e36b046fd5c0655007037a25bf97aa835d6e
Saurabh Gawade <saurabhgawade2920@gmail.com>
2024-01-24 23:43:38 +0530

    update

--- client/src/pages/Contact.jsx
+++ client/src/pages/Contact.jsx
@@ -1,5 +1,62 @@
+import contactImg from "../assets/contact.png";
+
 const Contact = () => {
-  return <div>Contact</div>;
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+  return (
+    <>
+      <div className="container">
+        <div className="row">
+          <div className="col-12 col-sm-12 col-md-6 col-lg-6 my-5">
+            <div className="wrap-register-img">
+              <img src={contactImg} alt="" className="img-fluid" />
+            </div>
+          </div>
+          <div className="col-12 col-sm-12 col-md-6 col-lg-6 my-5">
+            <h3 className="display-6 text-white fw-bold mb-3">Contact Now</h3>
+            <form className="" onSubmit={handleSubmit}>
+              <div className="form-floating mb-3 text-dark">
+                <input
+                  required
+                  type="text"
+                  className="form-control"
+                  id="floatingInput1"
+                  placeholder="Enter Username"
+                />
+                <label htmlFor="floatingInput1">Username</label>
+              </div>
+              <div className="form-floating mb-3">
+                <input
+                  required
+                  type="email"
+                  className="form-control"
+                  id="floatingInput2"
+                  placeholder="name@example.com"
+                />
+                <label htmlFor="floatingInput2">Email address</label>
+              </div>
+
+              <div className="form-floating mb-3">
+                <textarea
+                  rows="6"
+                  required
+                  type="text"
+                  className="form-control h-100"
+                  id="floatingPassword4"
+                  placeholder="Enter Msg Here"
+                ></textarea>
+                <label htmlFor="floatingPassword4">Message</label>
+              </div>
+              <button type="submit" className="myBTN">
+                Log In
+              </button>
+            </form>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Contact;
--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,5 +1,51 @@
+import loginImg from "../assets/login.png";
+
 const Login = () => {
-  return <div>Login</div>;
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+  return (
+    <>
+      <div className="container">
+        <div className="row">
+          <div className="col-12 col-sm-12 col-md-6 col-lg-6 my-5">
+            <div className="wrap-register-img">
+              <img src={loginImg} alt="" className="img-fluid" />
+            </div>
+          </div>
+          <div className="col-12 col-sm-12 col-md-6 col-lg-6 my-5">
+            <h3 className="display-6 text-white fw-bold mb-3">Login Now</h3>
+            <form className="" onSubmit={handleSubmit}>
+              <div className="form-floating mb-3">
+                <input
+                  required
+                  type="email"
+                  className="form-control"
+                  id="floatingInput2"
+                  placeholder="name@example.com"
+                />
+                <label htmlFor="floatingInput2">Email address</label>
+              </div>
+
+              <div className="form-floating mb-3">
+                <input
+                  required
+                  type="password"
+                  className="form-control"
+                  id="floatingPassword4"
+                  placeholder="Password"
+                />
+                <label htmlFor="floatingPassword4">Password</label>
+              </div>
+              <button type="submit" className="myBTN">
+                Log In
+              </button>
+            </form>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Login;
