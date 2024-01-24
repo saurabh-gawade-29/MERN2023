0099690819caeb558fcffece59ca075889fca955
saurabh gawade <saurabhgawade@finacus.co.in>
2024-01-24 11:59:57 +0530

    ZOD Validation added

--- server/router/auth-router.js
+++ server/router/auth-router.js
@@ -3,12 +3,14 @@ const router = express.Router();
 //! Named Export
 // const { home, register, login } = require("../controllers/auth-controller");
 const authControllers = require("../controllers/auth-controller");
+const validate = require("../middlewares/validate-middleware");
+const signupSchema = require("../validators/auth-validator");
 
 //TODO: http://localhost:5000/api/auth/
 router.route("/").get(authControllers.home);
 
 //TODO: http://localhost:5000/api/auth/register
-router.route("/register").post(authControllers.register);
+router.route("/register").post(validate(signupSchema), authControllers.register);
 
 //TODO: http://localhost:5000/api/auth/login
 router.route("/login").post(authControllers.login);
---
+++ server/validators/auth-validator.js
@@ -0,0 +1,36 @@
+const { z } = require("zod");
+
+//! Creating Object Scehma for validation
+const signupSchema = z.object({
+  username: z
+    .string({
+      required_error: "Name is Required",
+    })
+    .trim()
+    .min(3, { message: "Username must be at least 3 Charachter" })
+    .max(255, {
+      message: "Username must not be more than 255 Charachter",
+    }),
+  email: z
+    .string({
+      required_error: "Email is Required",
+    })
+    .trim()
+    .email({ message: "Invalid Email Address" })
+    .min(3, { message: "Email must be at least 3 Chars" })
+    .max(255, {
+      message: "Email must not be more than 255 Charachter",
+    }),
+  phone: z
+    .string({
+      required_error: "Phone is Required",
+    })
+    .trim()
+    .email({ message: "Invalid Phone Number" })
+    .min(10, { message: "Phone must be at least 10 Charachter" })
+    .max(20, {
+      message: "Phone must not be more than 20 Charachter",
+    }),
+});
+
+module.exports = signupSchema;
